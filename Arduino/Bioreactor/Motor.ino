85e8218c92ca990f9de601e7b2a9565c87a38b1e
Chak Koppula <chak.koppula199@gmail.com>
2018-11-21 12:25:32 +0000

    Uploaded code in porgress for motor control: upload 1

--- Arduino/Bioreactor/Motor.ino
+++ Arduino/Bioreactor/Motor.ino
@@ -1,18 +1,68 @@
 // Readonly Variables:
-// float targetMotorSpeed;
+float targetMotorSpeed;
 // float targetPH;
 // float targetTemperature;
-// float motorSpeed;
+float motorSpeed;
 // float ph;
 // float temperature;
 
 // Functions:
-// void sendReading(int sensor, float value)
-// void sendDebug(char *message)
-
-void motor_setup(){
+void sendReading(int sensor, float value) {
   
 }
+// void sendDebug(char *message)
+
+void checkRequiredRPM {
+  requiredRPM = targetMotorSpeed;
+}
+void getPhotoInterrupt() {
+  beginTime = millis();
+  while(timePassed < 5000) {
+    endTime = millis();
+    timePassed = endTime - beginTime;
+    photoSensorValue = analogRead(photoPin);
+    //check if sensor detected change
+    //if change detected increment photoInterruptCount
+  }
+  timePassed = 0;
+}
+
+void calculateRPM() {
+  photoInterruptCount = photoInterruptCount / 2;
+  RPM = photoInterruptCount * 12;
+}
+
+void checkRPM() {
+  if(RPM < (requiredRPM - 20)) {
+    //speed up
+  }
+  else if(RPM > (requiredRPM + 20)) {
+    //slow down
+  }
+  else {
+    continue;
+  }
+}
+
+void motor_setup(){
+
+  const int photoPin = A0;
+  
+  int photoInterruptCount = 0;
+  int photoSensorValue = 0;
+  
+  float RPM = 0;
+  float requiredRPM = 0;
+  
+  int beginTime = 0;
+  int endTime = 0;
+  int timePassed = 0;
+}
 void motor_loop(){
+
+  checkRequiredRPM();
+  getPhotoInterrupt();
+  calculateRPM();
+  checkRPM();
   
 }
