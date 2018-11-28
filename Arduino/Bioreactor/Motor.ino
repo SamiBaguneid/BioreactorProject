15256df80caac1110a964d685978176e9b4c4017
Chak Koppula <chak.koppula199@gmail.com>
2018-11-28 12:39:28 +0000

    Added updated motor imo

--- Arduino/Bioreactor/Motor.ino
+++ Arduino/Bioreactor/Motor.ino
@@ -1,60 +1,64 @@
-// Readonly Variables:
-float targetMotorSpeed;
-// float targetPH;
-// float targetTemperature;
-// float motorSpeed;
-// float ph;
-// float temperature;
-
 // Functions:
 // void sendReading(int sensor, float value)
 // void sendDebug(char *message)
 
-void checkRequiredRPM {
+const int photoPin = A0;
+const int transistorPin = 9;
+
+int startValue = 150;
+  
+int photoInterruptCount = 0;
+int photoSensorValue = 0;
+  
+double RPM = 0;
+float requiredRPM = 0;
+  
+long beginTime = 0;
+long endTime = 0;
+long timePassed = 0;
+
+void checkRequiredRPM() {
   requiredRPM = targetMotorSpeed;
 }
+
 void getPhotoInterrupt() {
   beginTime = millis();
-  while(timePassed < 5000) {
+  while(timePassed < 2000) {
     endTime = millis();
     timePassed = endTime - beginTime;
     photoSensorValue = analogRead(photoPin);
-    //check if sensor detected change
-    //if change detected increment photoInterruptCount
+    photoSensorValue = photoSensorValue * (5.0/1023);
+    if (photoSensorValue < 2) {
+      photoInterruptCount += 1;
+      while(photoSensorValue < 2) {
+        photoSensorValue = analogRead(photoPin);
+        photoSensorValue = photoSensorValue * (5.0/1023);
+      }
+    }
   }
   timePassed = 0;
 }
 
 void calculateRPM() {
   photoInterruptCount = photoInterruptCount / 2;
-  RPM = photoInterruptCount * 12;
+  RPM = photoInterruptCount * 30;
+  photoInterruptCount = 0;
 }
 
 void checkRPM() {
-  if(RPM < (requiredRPM - 20)) {
-    //speed up using PWM
+  if(RPM < (requiredRPM - 15)) {
+    startValue += 10;
+    analogWrite(transistorPin, startValue);
   }
-  else if(RPM > (requiredRPM + 20)) {
-    //slow down using PWM
-  }
-  else {
-    continue;
+  else if(RPM > (requiredRPM + 15)) {
+    startValue -= 10;
+    analogWrite(transistorPin, startValue);
   }
 }
 
 void motor_setup(){
-
-  const int photoPin = A0;
-  
-  int photoInterruptCount = 0;
-  int photoSensorValue = 0;
-  
-  float RPM = 0;
-  float requiredRPM = 0;
-  
-  int beginTime = 0;
-  int endTime = 0;
-  int timePassed = 0;
+  pinMode(transistorPin, OUTPUT);
+  Serial.println("Motor setup executed");
 }
 void motor_loop(){
 
@@ -62,6 +66,6 @@ void motor_loop(){
   getPhotoInterrupt();
   calculateRPM();
   checkRPM();
-  sendReading(1, RPM);
+  sendReading(0, RPM);
   
 }
